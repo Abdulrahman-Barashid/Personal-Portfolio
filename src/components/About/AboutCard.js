38b4bb7333642f607efd3b8ccb841d981600248d
Abdulrahman-Barashid <161174970+Abdulrahman-Barashid@users.noreply.github.com>
2024-02-26 01:47:16 +0300

    Add files via upload

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,23 +8,25 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Abdulrahman Barashid </span>
+            from <span className="purple"> Jeddah, Saudi Arabia.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am an IT student at King Abdulaziz University's Faculty of Computing and Information Technology (FCIT).
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I'm passion to explore various aspects of IT, and devolop software that would contrbiute to change my country.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing Video Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Reading
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Playing Football
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -34,7 +36,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Abdulrahman Barashid</footer>
         </blockquote>
       </Card.Body>
     </Card>
